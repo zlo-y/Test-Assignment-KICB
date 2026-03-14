0a9839cc49dd2c0494c30937acdbbb8a576c5814
zlo-y <kratosonewarrior@gmail.com>
2026-03-14 13:54:46 +0600

    feat(front): разработка интерфейса на Blazor WebAssembly
    - Реализован CRUD для пользователей и телефонов.
    - Настроена интеграция с API через HttpClient.
    - Внедрена фильтрация контактов по UserId через параметры URL.
    - Добавлена нормализация данных и обработка ошибок валидации.

---
+++ ContactGate.Client/Program.cs
@@ -0,0 +1,14 @@
+    using Microsoft.AspNetCore.Components.Web;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using ContactGate.Client;
+
+    var builder = WebAssemblyHostBuilder.CreateDefault(args);
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+
+    builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri("http://localhost:5000/") 
+});
+
+    await builder.Build().RunAsync();
